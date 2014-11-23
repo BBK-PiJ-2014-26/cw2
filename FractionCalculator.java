6836e44d9e7fff779b3a13f615ba3c4892fa1ba1
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-23 23:31:51 +0000

    almost complete, without testing

--- FractionCalculator.java
+++ FractionCalculator.java
@@ -10,11 +10,6 @@ public class FractionCalculator {
 	private boolean divide;
 	private boolean multiply;	
 
-	public FractionCalculator() {
-		this.f1 = null;
-		this.f2 = null;
-	}
-
 	public void launch() {
 		System.out.println("0");
 		this.operatorPresent = false;
@@ -22,6 +17,8 @@ public class FractionCalculator {
 		this.subtract = false;
 		this.divide = false;
 		this.multiply = false;
+		this.f1 = null;
+		this.f2 = null;
 		Scanner input = new Scanner(System.in);
 		do {
 			String s = input.next();
@@ -29,6 +26,8 @@ public class FractionCalculator {
 				this.setF1(s);
 			} else if (!operatorPresent) {
 				this.setOperator(s);
+			} else {
+				this.setF2(s);
 			}	
 		} while (input.hasNext());
 	}
@@ -93,13 +92,85 @@ public class FractionCalculator {
 				this.multiply = true;
 				this.operatorPresent = true;
 			} else {
-				this.error();
+				this.specialOperator(s);
 			}
 		} else {
 			this.error();
 		}
 	}
-	
+
+	public void specialOperator(String s) {
+		if (s.equals("a") || s.equals("A") || s.equals("abs")) {
+			this.f1 = this.f1.absValue();
+		} else if (s.equals("n") || s.equals("N") || s.equals("neg")) { 
+			this.f1 = this.f1.negate();
+		} else if (s.equals("c") || s.equals("C") || s.equals("clear")) {
+			this.f1 = null;
+			System.out.println("0");
+		}
+	}
+
+	public void setF2(String s) {		
+		int sLength = s.length();
+		int count = 0;
+		int slashPos = 0;
+		boolean slashPresent = false;
+		for (count = 0; count < sLength; count++) {
+			char slash = s.charAt(count);
+			if (slash == ('/')) {
+				slashPos = count;
+				slashPresent = true;
+			}
+		}
+		if (slashPresent) {
+			String temp = s.substring(0, slashPos);
+			int num = Integer.parseInt(temp);
+			temp = s.substring((slashPos + 1), (sLength - 1)); 
+			int denom = Integer.parseInt(temp);
+			this.f2 = new Fraction(num, denom);
+
+		} else {
+			String temp = s.substring(0, (sLength - 1));
+			int num = Integer.parseInt(temp);
+			int denom = 1;
+			this.f2 = new Fraction(num, denom);
+		}
+		if (add) {
+			this.f1 = this.f1.add(this.f2);
+			this.f2 = null;
+			this.operatorPresent = false;
+			this.add = false;
+			this.subtract = false;
+			this.divide = false;
+			this.multiply = false;
+		} else if (subtract) {
+			this.f1 = this.f1.subtract(this.f2);
+			this.f2 = null;
+			this.operatorPresent = false;
+			this.add = false;
+			this.subtract = false;
+			this.divide = false;
+			this.multiply = false;	
+		} else if (divide) {
+			this.f1 = this.f1.divide(this.f2);
+			this.f2 = null;
+			this.operatorPresent = false;
+			this.add = false;
+			this.subtract = false;
+			this.divide = false;
+			this.multiply = false;
+		} else if (multiply) {
+			this.f1 = this.f1.multiply(this.f2);
+			this.f2 = null;
+			this.operatorPresent = false;
+			this.add = false;
+			this.subtract = false;
+			this.divide = false;
+			this.multiply = false;
+		} else {
+			this.error();
+		}
+	}	
 	
 }
 
