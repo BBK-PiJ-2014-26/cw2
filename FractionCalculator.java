82a88ec426b85f2628eda53b2d8c63c437d7821c
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2014-11-23 16:45:05 +0000

    first commit

---
+++ FractionCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FractionCalculator {
+
+	private Fraction f1;
+	private Fraction f2;
+	private String operator;
+	public static int start = 0;
+
+	public void launch() {
+		System.out.println(start);
+	}
+}
+
